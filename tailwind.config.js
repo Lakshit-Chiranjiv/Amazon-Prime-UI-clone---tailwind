8f81a69373b75f4042f812ec83422ee162207118
Lakshit-Chiranjiv <luckyhacker017@gmail.com>
2022-03-29 23:50:52 +0530

    Designed the sign in page

--- tailwind.config.js
+++ tailwind.config.js
@@ -1,6 +1,9 @@
 module.exports = {
   // content: ["./src/**/*.{html,js}"],
-  content: ['./*/*.{html,js}', ],
+  // content: ['./*/*.{html,js}', ],
+  content: [
+    './public/**/*.{html,js}'
+  ],
   theme: {
     extend: {},
   },
